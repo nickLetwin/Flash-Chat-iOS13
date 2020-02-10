7fedc7fca2c27a9106aadbd9859c0a2d827ce81f
Nick Letwin <nick@salonscale.com>
2020-02-10 14:03:39 -0600

    Added a custom cell to the table view for the ChatViewController, added keyboard manager. Chat now auto-scrolls to bottom when launched. - Nick L

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,9 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages: [Message] = [
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "Hello!"),
-        Message(sender: "1@2.com", body: "What's up?")
-    ]
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +27,71 @@ class ChatViewController: UIViewController {
         
         //Register the nib with the table view
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        //Load messages
+        loadMessages()
+    }
+    
+    func loadMessages()
+    {
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+            
+            if let e = error
+            {
+                print("There was an issue retrieving data from Firestore. \(e)")
+            }
+            else
+            {
+                if let snapshotDocuments = querySnapshot?.documents
+                {
+                    for doc in snapshotDocuments
+                    {
+                        let data = doc.data()
+                        if let sender = data[K.FStore.senderField] as? String,
+                           let body = data[K.FStore.bodyField] as? String
+                        {
+                            let message = Message(sender: sender, body: body)
+                            self.messages.append(message)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                self.scrollToBottom()
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email
+        {
+            messageTextfield.text = ""
+            
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error
+                {
+                    print("There was an problem saving data to Firestore, \(e)")
+                }
+                else
+                {
+                    print("Successfully saved data to Firestore")
+                }
+            }
+        }
+        
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -44,6 +104,14 @@ class ChatViewController: UIViewController {
           print ("Error signing out: %@", signOutError)
         }
           
+    }
+    
+    func scrollToBottom()
+    {
+        DispatchQueue.main.async {
+            let indexPath = IndexPath(row: self.messages.count-1, section: 0)
+            self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+        }
     }
     
 }
